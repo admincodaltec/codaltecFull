622d7063a9e73a01de96a839cfaadebdfd2c642f
Kosthon <alejofova@gmail.com>
2024-06-05 11:24:48 -0500

    reparación de las rutas con prefijo /es

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -2,9 +2,24 @@ const createNextIntlPlugin = require('next-intl/plugin');
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	async rewrites() {
+		return [
+			// Reescribe las rutas en inglés con prefijo /en
+			{
+				source: '/en/:path*',
+				destination: '/en/:path*',
+				locale: false,
+			},
+			// Reescribe las rutas sin prefijo (español por defecto)
+			{
+				source: '/:path*',
+				destination: '/:path*',
+				locale: false,
+			},
+		];
+	},
 };
 
 module.exports = withNextIntl(nextConfig);
