291e9d3b4e9dc389dfea4fb698027cb9479ff98c
Kosthon <alejofova@gmail.com>
2024-06-10 18:18:43 -0500

    Configuración

--- app/frontend/src/services/UserServices.tsx
+++ app/frontend/src/services/UserServices.tsx
@@ -6,13 +6,13 @@ export interface User {
 
 export async function getUserByEmail(email: string): Promise<User | null> {
 	try {
-		const response = await fetch(`${process.env.API_URL}users?email=${email}`);
+		const response = await fetch(`https://codaltec.com/admin/users?email=${email}`);
 		console.log(response);
 
 		if (!response.ok) {
 			throw new Error('Network response was not ok');
 		}
-        if (response.status === 404) {
+		if (response.status === 404) {
 			return null;
 		}
 		const data = await response.json();
