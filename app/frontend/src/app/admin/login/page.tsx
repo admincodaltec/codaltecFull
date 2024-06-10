1d2be3e47c66e8301994c6d41d30c35ecd7d7ba0
Kosthon <alejofova@gmail.com>
2024-06-10 18:12:17 -0500

    Configuración

--- app/frontend/src/app/admin/login/page.tsx
+++ app/frontend/src/app/admin/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {useState} from 'react';
+import {getUserByEmail} from '@/services/UserServices';
 
 export default function Login() {
 	const [email, setEmail] = useState('');
@@ -9,24 +10,28 @@ export default function Login() {
 	const handleSubmit = async (e: {preventDefault: () => void}) => {
 		e.preventDefault();
 
-		const response = await fetch('/api/auth/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-			}),
-		});
-
-		if (!response.ok) {
-			const errorMessage = `Error: ${response.status} - ${response.statusText}`;
-			console.error(errorMessage);
-			return;
+		const currentEmail = getUserByEmail(email);
+		console.log(currentEmail);
+		if (!currentEmail) {
+			alert('User not found');
+		} else {
+			const response = await fetch('/api/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					email,
+				}),
+			});
+			if (!response.ok) {
+				const errorMessage = `Error: ${response.status} - ${response.statusText}`;
+				console.error(errorMessage);
+				return;
+			}
+			const data = await response.json();
+			console.log(data);
 		}
-
-		const data = await response.json();
-		console.log(data);
 	};
 
 	return (
